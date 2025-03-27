101de7a8cf469c009ffbedfcf98a6e09d9255d94
Huy Nguyen <giahuy2201@icloud.com>
2025-03-27 16:17:21 -0400

    Fix ssh key copying

--- bootstrap-user.sh
+++ bootstrap-user.sh
@@ -1,7 +1,3 @@
-echo "------------ Copy ssh key to user ------------"
-cd ~ && \
-mkdir -p .ssh && \
-cp /root/.ssh/authorized_keys .ssh
 echo "------------ Get dotfiles ------------"
 git clone https://github.com/giahuy2201/dotfiles.git && \
 cd dotfiles && \
--- bootstrap.sh
+++ bootstrap.sh
@@ -9,6 +9,10 @@ echo "------------ Set up user with passwordless sudo ------------"
 useradd -m $MY_USER && \
 apt install sudo -y && \
 echo "$MY_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$MY_USER
+echo "------------ Copy ssh key to user ------------"
+mkdir -p /home/$MY_USER/.ssh && \
+cp /root/.ssh/authorized_keys .ssh && \
+chown -R $MY_USER:$MY_USER /home/$MY_USER
 echo "------------ Bootstrap as user ------------"
 curl -sL $USER_SCRIPT | sudo -u $MY_USER bash
 echo "------------ Bootstrapping finished ------------"
