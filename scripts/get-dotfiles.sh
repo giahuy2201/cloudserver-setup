7ca1875cce6a804a58f3ad1f97b3458647c87aaa
Huy Nguyen <giahuy2201@icloud.com>
2025-03-31 23:20:54 -0400

    Update get-dotfiles stow

--- scripts/get-dotfiles.sh
+++ scripts/get-dotfiles.sh
@@ -1,5 +1,6 @@
 cd ~ && \
 git clone https://github.com/giahuy2201/dotfiles.git && \
 cd dotfiles && \
-stow -v -t $HOME */ && \
+stow -v -t $HOME --adopt */ && \
+git checkout . && \
 source $HOME/.bashrc
