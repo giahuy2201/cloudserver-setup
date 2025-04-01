bad86a05b975e16bcedbaa66183fc9d55914ce71
Huy Nguyen <giahuy2201@icloud.com>
2025-04-01 01:54:30 -0400

    Set user uid and gid

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -6,7 +6,7 @@ echo "------------ Upgrade and install packages ------------"
 apt update && \
 apt install neovim neofetch git screen btop htop stow -y
 echo "------------ Set up user with passwordless sudo ------------"
-useradd -m -s "$(which bash)" $MY_USER
+useradd -m -s "$(which bash)" -u 1000 -g 1000 $MY_USER
 apt install sudo -y && \
 echo "$MY_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$MY_USER
 echo "------------ Copy ssh key to user ------------"
