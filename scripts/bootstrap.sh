e7a50d0418e58fdd370427896f4dfb02f545e49c
Huy Nguyen <giahuy2201@icloud.com>
2025-04-11 00:40:51 -0400

    Remove SSH copying from bootstrap script

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -9,10 +9,6 @@ echo "------------ Set up user with passwordless sudo ------------"
 useradd -m -s "$(which bash)" -u 1000 -g 1000 $MY_USER
 apt install sudo -y && \
 echo "$MY_USER ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/$MY_USER
-echo "------------ Copy ssh key to user ------------"
-mkdir -p /home/$MY_USER/.ssh && \
-cp /root/.ssh/authorized_keys .ssh && \
-chown -R $MY_USER:$MY_USER /home/$MY_USER
 echo "------------ Bootstrap as user ------------"
 curl -sL $USER_SCRIPT | sudo -u $MY_USER bash
 echo "------------ Bootstrapping finished ------------"
